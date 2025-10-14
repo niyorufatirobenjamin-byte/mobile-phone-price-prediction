0ba9de430de85a089cf18c96a532824062e68cb9
niyorufatirobenjamin-byte <niyorufatirobenjamin@gmail.com>
2025-10-14 19:06:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,20 +14,20 @@ def phone_price_prediction(Brand, Battery_capacity_mAh, Screen_size_inches,
                             Rear_camera, Operating_system, Number_of_SIMs,
                             three_G, four_G_LTE):
     # Create DataFrame from input
-    new_phone = pd.DataFrame([{
-        'Brand': Brand,
-        'Battery capacity (mAh)': Battery_capacity_mAh,
-        'Screen size (inches)': Screen_size_inches,
-        'Touchscreen': Touchscreen,
-        'Processor': Processor,
-        'RAM (MB)': RAM_MB,
-        'Internal storage (GB)': Internal_storage_GB,
-        'Rear camera': Rear_camera,
-        'Operating system': Operating_system,
-        'Number of SIMs': Number_of_SIMs,
-        '3G': three_G,
-        '4G/ LTE': four_G_LTE
-    }])
+   new_phone = pd.DataFrame([{
+    'Brand': 44,
+    'Battery capacity (mAh)': 4085,
+    'Screen size (inches)': 6.67,
+    'Touchscreen': 1,
+    'Processor': 8,
+    'RAM (MB)': 12000,
+    'Internal storage (GB)': 256.0,
+    'Rear camera': 48.0,
+    'Operating system': 0,
+    'Number of SIMs': 2,
+    '3G': 1,
+    '4G/ LTE': 1
+}])
     
     # Predict price
     predicted_price = loaded_model.predict(new_phone)
@@ -83,4 +83,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
